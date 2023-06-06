9140b27ec468c6bf1931999c08a948d23715770c
Graziela Santos <134293281+Grazizpzp@users.noreply.github.com>
2023-06-06 11:07:39 -0300

    Add files via upload
    Configuração inicial do portfólio no github

---
+++ Portfolfio/main.js
@@ -0,0 +1,7 @@
+[Desktop Entry]
+Version=1.0
+Type=Link
+Name=main.jsJavaScript
+Comment=
+Icon=user-bookmarks
+URL=https://drive.google.com/file/d/13OEd6e7tQP4VIbv3to8Y4qCGKC3luwkE/view?usp=drive_web&authuser=0
